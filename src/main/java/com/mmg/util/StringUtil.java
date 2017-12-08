822756208c2386fc549f79f690dce3054de4ab96
zhuyajun <zhuyajun@yupiao.com>
2017-12-08 17:47:12 +0800

    zyj

--- src/main/java/com/mmg/util/StringUtil.java
+++ src/main/java/com/mmg/util/StringUtil.java
@@ -13,14 +13,34 @@ import sun.misc.BASE64Encoder;
 public class StringUtil {
     private final static  Log logger = LogFactory.getLog(StringUtil.class);
     @SuppressWarnings("restriction")
-	public static String getMd5Stri(String str){
-        try{
-            MessageDigest md5 = MessageDigest.getInstance("MD5");
-            BASE64Encoder base64en = new BASE64Encoder();
-            return base64en.encode(md5.digest(str.getBytes("utf-8")));
-        }catch (Exception ex){
-            logger.error(ex.getMessage());
+    public static String getMd5Stri(String message) {
+        String md5 = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");  // 创建一个md5算法对象
+            byte[] messageByte = message.getBytes("UTF-8");
+            byte[] md5Byte = md.digest(messageByte);              // 获得MD5字节数组,16*8=128位
+            md5 = bytesToHex(md5Byte);                            // 转换为16进制字符串
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return str;
+        return md5;
     }
+
+    // 二进制转十六进制
+    public static String bytesToHex(byte[] bytes) {
+        StringBuffer hexStr = new StringBuffer();
+        int num;
+        for (int i = 0; i < bytes.length; i++) {
+            num = bytes[i];
+            if(num < 0) {
+                num += 256;
+            }
+            if(num < 16){
+                hexStr.append("0");
+            }
+            hexStr.append(Integer.toHexString(num));
+        }
+        return hexStr.toString().toUpperCase();
+    }
+
 }
